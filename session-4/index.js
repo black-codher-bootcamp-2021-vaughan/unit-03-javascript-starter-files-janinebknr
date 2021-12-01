5226194ab4ff756a957731efcade03a7d746bfb2
janinebknr <87192949+janinebknr@users.noreply.github.com>
2021-12-01 19:13:31 +0000

    Adding biography function

--- session-4/index.js
+++ session-4/index.js
@@ -1,20 +1,49 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA = {
+  name: "Shan",
+  age: 25,
+  location: "Birmingham",
+  likes: "singing",
+};
 
+let personB = {
+  name: "Dom",
+  age: 33,
+  location: "London",
+  likes: "cooking",
+};
 
+let personC = {
+  name: "Ri",
+  age: 26,
+  location: "Birmingham",
+  likes: "playing in makeup",
+};
 
+let personD = {
+  name: "Dani",
+  age: 30,
+  location: "Walsall",
+  likes: "watching movies",
+};
 
+function biography(person) {
+  return (
+    "Hi, my name is " +
+    person.name +
+    ". I am " +
+    person.age +
+    " years old, I live in " +
+    person.location +
+    " and I like " +
+    person.likes
+  );
+}
 
-
-
-
-
-
-
-
-
-
-
-
+console.log(biography(personA));
+console.log(biography(personB));
+console.log(biography(personC));
+console.log(biography(personD));
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
@@ -38,33 +67,4 @@
 
 // console.log("Division: x + y " + division);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ****************************************************TASK 3****************************************************
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
