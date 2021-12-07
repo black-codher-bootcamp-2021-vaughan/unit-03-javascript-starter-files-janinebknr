6549fec98ec3c7a3b7c5ef4b4d3f69ecbe06482b
janinebknr <87192949+janinebknr@users.noreply.github.com>
2021-12-07 21:33:37 +0000

    Updating getGrade function to use SWITCH statement

--- session-5/index.js
+++ session-5/index.js
@@ -1,21 +1,52 @@
-/* TASK 1 - Grading Function */
+/* TASK 1 - Grading Function IF statements */
+// function getGrade(total) {
+//   if (total > 90) {
+//     return "A+";
+//   } else if (total > 80 && total <= 90) {
+//     return "A";
+//   } else if (total > 70 && total <= 80) {
+//     return "B";
+//   } else if (total > 60 && total <= 70) {
+//     return "C";
+//   } else if (total > 50 && total <= 60) {
+//     return "D";
+//   } else if (total > 40 && total <= 50) {
+//     return "E";
+//   } else if (total > 30 && total <= 40) {
+//     return "F";
+//   } else {
+//     return "Fail";
+//   }
+// }
+
+// console.log(getGrade(55));
+
+/* TASK 2 - Grading Function SWITCH statement */
 function getGrade(total) {
-  if (total > 90) {
-    return "A+";
-  } else if (total > 80 && total <= 90) {
-    return "A";
-  } else if (total > 70 && total <= 80) {
-    return "B";
-  } else if (total > 60 && total <= 70) {
-    return "C";
-  } else if (total > 50 && total <= 60) {
-    return "D";
-  } else if (total > 40 && total <= 50) {
-    return "E";
-  } else if (total > 30 && total <= 40) {
-    return "F";
-  } else {
-    return "Fail";
+  switch (true) {
+    case total > 90:
+      return "A+";
+      break;
+    case total > 80 && total <= 90:
+      return "A";
+      break;
+    case total > 70 && total <= 80:
+      return "B";
+      break;
+    case total > 60 && total <= 70:
+      return "C";
+      break;
+    case total > 50 && total <= 60:
+      return "D";
+      break;
+    case total > 40 && total <= 50:
+      return "E";
+      break;
+    case total > 30 && total <= 40:
+      return "F";
+      break;
+    default:
+      return "Fail";
   }
 }
 
